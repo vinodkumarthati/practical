bc661c1b7ad4afad5ae8b4592f90e648accbf233
vinnu <vinod@vinods-MacBook-Air.local>
2025-12-01 17:13:28 +0530

    1

---
+++ exp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class exp{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int sum=a+b;
+        System.out.println("The sum is: " + sum);
+    }
+}   
